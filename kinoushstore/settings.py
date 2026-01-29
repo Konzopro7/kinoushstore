d1c4a2f5acc892c36f9a0bf6a623d7285fed3239
konzo tech <mouhamadoukonare99@gmail.com>
2026-01-29 12:34:57 -0500

    Serve static via WhiteNoise finders

--- kinoushstore/settings.py
+++ kinoushstore/settings.py
@@ -160,7 +160,8 @@ if not DEBUG:
     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
     CSRF_COOKIE_SECURE = True
     SESSION_COOKIE_SECURE = True
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+    WHITENOISE_USE_FINDERS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
